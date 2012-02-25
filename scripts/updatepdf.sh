4e953fd512c1278f407990cd9c779c55122d7497
Lorenz H-S <lorenz-uni@lgh-alumni.de>
2012-02-25 18:52:24 +0100

    Update Chromium pdf-plugin installer

--- scripts/updatepdf.sh
+++ scripts/updatepdf.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 # Extracts the Chrome PDF viewer and copies it so Chromium uses it
+# Adapt this if your chromium is located e.g. at /usr/lib/chromium-browser
+CHROMIUMPATH=/usr/lib/chromium
 
 TEMPDIR=`mktemp -d`
 pushd $TEMPDIR
@@ -7,7 +9,7 @@ echo "Downloading current chrome amd64 beta..."
 wget "http://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb"
 echo "Extracting..."
 dpkg -x google-chrome-beta_current_amd64.deb .
-echo "Copying libpdf.so to /usr/lib/chromium-browser..."
-sudo cp opt/google/chrome/libpdf.so /usr/lib/chromium-browser/
+echo "Copying libpdf.so to $CHROMIUMPATH..."
+sudo cp opt/google/chrome/libpdf.so $CHROMIUMPATH
 popd
 rm -r $TEMPDIR
