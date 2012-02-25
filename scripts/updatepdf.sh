30f614943712cc41af0bd8966d6018457f9fcae5
Lorenz H-S <lorenz-uni@lgh-alumni.de>
2012-02-25 19:32:29 +0100

    Make updatepdf.sh more general (architecture/version). Still uses dpkg, though

--- scripts/updatepdf.sh
+++ scripts/updatepdf.sh
@@ -2,14 +2,27 @@
 # Extracts the Chrome PDF viewer and copies it so Chromium uses it
 # Adapt this if your chromium is located e.g. at /usr/lib/chromium-browser
 CHROMIUMPATH=/usr/lib/chromium
+# Choose one: unstable, beta, stable
+CRELEASE=beta
+
+ARCHSTRING=$(uname -m)
+if [[ $ARCHSTRING == "x86_64" ]]; then
+	ARCH=amd64
+elif [[ $ARCHSTRING == "i686" ]]; then
+	ARCH=i386
+else
+	echo "could not determine platform"
+	return 1
+fi
 
 TEMPDIR=`mktemp -d`
 pushd $TEMPDIR
-echo "Downloading current chrome amd64 beta..."
-wget "http://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb"
+echo "Downloading current chrome ${CRELEASE} for ${ARCH}..."
+wget "http://dl.google.com/linux/direct/google-chrome-${CRELEASE}_current_${ARCH}.deb"
 echo "Extracting..."
-dpkg -x google-chrome-beta_current_amd64.deb .
-echo "Copying libpdf.so to $CHROMIUMPATH..."
-sudo cp opt/google/chrome/libpdf.so $CHROMIUMPATH
+# If you're not using a system with Debian package management, you might want to use ar xv and lzma -df and tar xvf instead
+dpkg -x google-chrome-${CRELEASE}_current_${ARCH}.deb .
+echo "Installing libpdf.so to ${CHROMIUMPATH}..."
+sudo install -Dm 755 ./opt/google/chrome/libpdf.so ${CHROMIUMPATH}/libpdf.so
 popd
 rm -r $TEMPDIR
