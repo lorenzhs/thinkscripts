3d8fd9ee135c25734ccb34741fd6386a804f17a2
Lorenz H-S <lorenz-dev@lgh-alumni.de>
2012-06-20 16:29:09 +0200

    [lock] forgot reference to timo ;)

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # This neat little script displays a full-screen image and locks your screen
+# Originally by Timo - https://github.com/timo
 # Dependencies: feh, xtrlock
 
 FILES=( ~/stuff/backgrounds/*(.) )
