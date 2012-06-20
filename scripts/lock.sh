6d6e5fd6816c5e05a7e728d0b4d2e8becbee8677
Lorenz H-S <lorenz-dev@lgh-alumni.de>
2012-06-20 15:50:04 +0200

    [lock] show a random picture from a folder

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 # This neat little script displays a full-screen image and locks your screen
 # Dependencies: feh, xtrlock
 
-feh -FZ ~/stuff/bg.jpg &
+FILES=( ~/stuff/backgrounds/*(.) )
+feh -FZ $FILES[$RANDOM%$#FILES+1] &
 PID=$!
 until xtrlock; do; done;
 kill $PID
