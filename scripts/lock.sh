7e80881eae8a34c1bcad2251f6e81b378385c945
Lorenz H-S <lorenz-dev@lgh-alumni.de>
2012-06-25 13:16:09 +0200

    [lock] Use feh's randomization feature
    This is more robust, since the previous implementation did not display an image if the randomized element happened not to be an image. Thanks for the tip, timo ;)

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
-# This neat little script displays a full-screen image and locks your screen
+# This neat little script displays a random image from a folder in fullscreen and locks your screen
 # Originally by Timo - https://github.com/timo
 # Dependencies: feh, xtrlock
 
-FILES=( ~/stuff/backgrounds/*(.) )
-feh -FZ $FILES[$RANDOM%$#FILES+1] &
+# Make sure you change this to a suitable location on your machine!
+FOLDER=~lorenz/stuff/backgrounds/
+
+# This part is machine-independent, no need to change it
+feh -zFZ $FOLDER &
 PID=$!
 until xtrlock; do; done;
 kill $PID
